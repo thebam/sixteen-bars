e748527a4d557cf2f5fa4ec3ea366200ccc1832b
thebam <jason.j.saunders@gmail.com>
2015-07-06 06:55:40 -0400

    Added TrackApiController to handle track autocomplete and track exists.

---
+++ sixteenBars.Tests/Controllers/TrackAPIControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sixteenBars.Tests.Model;
+using sixteenBars.Controllers;
+using System.Web.Mvc;
+using sixteenBars.Library;
+using System.Collections.Generic;
+
+namespace sixteenBars.Tests.Controllers
+{
+    [TestClass]
+    public class TrackAPIControllerTest
+    {
+        [TestMethod]
+        public void TrackAPI_TrackExists()
+        {
+            TrackAPIController ctrl = new TrackAPIController(new MockSixteenBarsDb());
+            Boolean result = ctrl.TrackExists("Light Up");
+            Assert.AreEqual(true, result, "Track 'Light Up' exists");
+
+            result = ctrl.TrackExists("Curls");
+            Assert.AreEqual(false, result, "Track 'Curls' doesn't exists");
+        }
+
+        [TestMethod]
+        public void TrackAPI_AutoComplete() {
+            TrackAPIController ctrl = new TrackAPIController(new MockSixteenBarsDb());
+            var result = ctrl.TrackAutoComplete("wet") as JsonResult;
+            List<Track> results = result.Data as List<Track>;
+            Assert.AreEqual(1, results.Count, "Wetter Than Tsunami not found");
+            Assert.AreEqual("Wetter Than Tsunami", results[0].Title, "Name not Wetter Than Tsunami");
+
+            result = ctrl.TrackAutoComplete("thing") as JsonResult;
+            results = result.Data as List<Track>;
+            Assert.AreEqual(0, results.Count, "Incorrect track found");
+           
+        }
+    }
+}
---
+++ sixteenBars/Controllers/TrackAPIController.cs
@@ -0,0 +1,45 @@
+﻿using sixteenBars.Library;
+using sixteenBars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace sixteenBars.Controllers
+{
+    public class TrackAPIController : ApiController
+    {
+        private ISixteenBarsDb _db;
+        public TrackAPIController() {
+            _db = new SixteenBarsDb();
+        }
+
+        public TrackAPIController(ISixteenBarsDb db) {
+            _db = db;
+        }
+
+        [System.Web.Http.HttpPost]
+        public bool TrackExists([FromBody]String trackName)
+        {
+            Track trackFound = _db.Tracks.SingleOrDefault(t => t.Title.ToLower() == trackName.Trim().ToLower());
+            if (trackFound == null) {
+                return false;
+            }else{
+                return true;
+            }
+        }
+
+        [System.Web.Http.HttpGet]
+        public JsonResult TrackAutoComplete(String title)
+        {
+            List<Track> tracks = _db.Tracks.Where(t => t.Title.ToLower().Contains(title.Trim().ToLower())).OrderBy(t => t.Title).ToList();
+            JsonResult result = new JsonResult();
+            result.Data = tracks;
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
+        }
+    }
+}
