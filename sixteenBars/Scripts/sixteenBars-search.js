bf2ea2b85af93a81b6856d7e66c9d943839e9e46
thebam <jason.j.saunders@gmail.com>
2015-09-26 14:47:06 -0400

    updated main search result formatting
    main search will now display a message if no results are found.
    
    main search will suggest turning explicit lyrics on if no results are found and the language toggle is set to clean

--- sixteenBars/Scripts/sixteenBars-search.js
+++ sixteenBars/Scripts/sixteenBars-search.js
@@ -4,6 +4,8 @@
 searchApp.controller('SearchController', function ($scope, $http) {
     $scope.searchTerm;
     $scope.searchType;
+    $scope.searchtermTitle = false;
+    $scope.searchNoResults = false;
                 
     $scope.search = function () {
         if ($scope.searchType == "" || $scope.searchType == undefined) {
@@ -21,10 +23,22 @@ searchApp.controller('SearchController', function ($scope, $http) {
         var baseUrl = siteURL + "/api/SearchAPI/Search?searchTerm=" + $scope.searchTerm + "&searchType=" + $scope.searchType + "&wordLink=false&filter=" + getCookie();
         var promise = $http.get(baseUrl);
         promise.then(function (payload) {
-            $scope.quotes = payload.data.Data;
+            $scope.searchtermTitle = true;
+            if (payload.data.Data.length > 0) {
+                $scope.quotes = payload.data.Data;
+                $scope.searchNoResults = false;
+            } else {
+
+                if (getCookie()==false) {
+                    alert('Try setting the language filter to "Explicit".');
+                }
+                $scope.searchNoResults = true;
+            }
         },
             function (errorPayload) {
                 $scope.quotes = "";
+                $scope.searchtermTitle = true;
+                $scope.searchNoResults = true;
             });
     };
 
