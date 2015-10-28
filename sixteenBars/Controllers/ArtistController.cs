ca594c95b766b829718baea9483006b2adf31a7e
thebam <jason.j.saunders@gmail.com>
2015-10-28 06:14:48 -0400

    In detail and namedetail of artist controller applied explicit filter

--- sixteenBars/Controllers/ArtistController.cs
+++ sixteenBars/Controllers/ArtistController.cs
@@ -7,6 +7,7 @@ using sixteenBars.Models;
 using PagedList;
 using System.Web.Script.Serialization;
 using WebMatrix.WebData;
+using System.Web;
 
 
 namespace sixteenBars.Controllers
@@ -61,7 +62,7 @@ namespace sixteenBars.Controllers
             ViewBag.MetaDescription = "Hip-Hop artist or rapper";
             ViewBag.MetaKeywords = "Hip-Hop, hip hop, artist, rapper, rap, music";
             //Create view model to display quotes and albums
-
+            HttpCookie isExplicit = Request.Cookies["explicit"];
             Artist artist = _db.Artists.Find(id);
             ArtistDetailViewModel artistVM = new ArtistDetailViewModel();
             if (artist == null)
@@ -78,7 +79,13 @@ namespace sixteenBars.Controllers
                 artistVM.Name = artist.Name;
                 artistVM.Albums = _db.Albums.Where(a
                     => a.Artist.Id == artist.Id).OrderBy(a => a.Title).ToList();
-                artistVM.Quotes = _db.Quotes.Where(q => q.Artist.Id == artist.Id).OrderBy(q => q.Text).ToList();
+                if (isExplicit.Value == "explicit")
+                {
+                    artistVM.Quotes = _db.Quotes.Where(q => q.Artist.Id == artist.Id).OrderBy(q => q.Text).ToList();
+                }
+                else {
+                    artistVM.Quotes = _db.Quotes.Where(q => q.Artist.Id == artist.Id && q.Explicit == false).OrderBy(q => q.Text).ToList();
+                }
             }
             return View(artistVM);
         }
@@ -88,7 +95,7 @@ namespace sixteenBars.Controllers
             ViewBag.Title = "Rhyme 4 Rhyme : Artist";
             ViewBag.MetaDescription = "Hip-Hop artist or rapper";
             ViewBag.MetaKeywords = "Hip-Hop, hip hop, artist, rapper, rap, music";
-
+            HttpCookie isExplicit = Request.Cookies["explicit"];
             artistname = URLClean.Clean(artistname);
 
             Artist artist = _db.Artists.SingleOrDefault(a => a.Name.Replace(".", "").Replace(",", "").Replace("&", "").Replace("?", "").Replace("%", "").Replace("!", "").Replace("*", "").Replace(":", "").Replace("<", "").Replace(">", "").Replace("\\", "").Replace(";", "").ToLower() == artistname.ToLower().Trim());
@@ -107,7 +114,14 @@ namespace sixteenBars.Controllers
                 artistVM.Id = artist.Id;
                 artistVM.Name = artist.Name;
                 artistVM.Albums = _db.Albums.Where(a => a.Artist.Id == artist.Id).OrderBy(a => a.Title).ToList();
-                artistVM.Quotes = _db.Quotes.Where(q => q.Artist.Id == artist.Id).OrderBy(q => q.Text).ToList();
+                if (isExplicit.Value == "explicit")
+                {
+                    artistVM.Quotes = _db.Quotes.Where(q => q.Artist.Id == artist.Id).OrderBy(q => q.Text).ToList();
+                }
+                else
+                {
+                    artistVM.Quotes = _db.Quotes.Where(q => q.Artist.Id == artist.Id && q.Explicit == false).OrderBy(q => q.Text).ToList();
+                }
             }
             return View("details", artistVM);
         }
